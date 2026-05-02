72b667070aeca6c70ba1adc360ea49a580b41c72
ayushlodh <ayushlodh2022@gmail.com>
2026-05-02 09:42:58 +0530

    Concurrency code

---
+++ src/concurrency/chef_and_waiter/RestaurantSimulation.java
@@ -0,0 +1,64 @@
+package concurrency.chef_and_waiter;
+
+class WaiterThread extends Thread {
+    private final Object lock;
+
+    public WaiterThread(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lock) {
+            try {
+                System.out.println("Waiter : Waiting for food to be ready... " + Thread.currentThread().getId());
+                lock.wait();
+                System.out.println("Waiter " + Thread.currentThread().getId() + " is ready to serve the customer.");
+                System.out.println("Waiter : Food is ready! Serving the customer.");
+            } catch (InterruptedException e) {
+                System.out.println("Waiter : Interrupted while waiting.");
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
+
+class ChefThread extends Thread {
+    private final Object lock;
+
+    public ChefThread(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+            synchronized (lock) {
+                System.out.println("Chef : Food is ready! Notifying the waiter...");
+                lock.notify();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Chef : Interrupted while preparing food.");
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+public class RestaurantSimulation {
+    public static void main(String[] args) {
+        Object lock = new Object();
+        Thread waiter = new WaiterThread(lock);
+        Thread waiter2 = new WaiterThread(lock);
+        Thread waiter3 = new WaiterThread(lock);
+        Thread chef = new ChefThread(lock);
+        Thread chef2 = new ChefThread(lock);
+        Thread chef3 = new ChefThread(lock);
+
+        waiter.start();
+        waiter2.start();
+        waiter3.start();
+        chef.start();
+        chef3.start();
+        chef2.start();
+    }
+}
---
+++ src/concurrency/thread_pools/ThreadPoolExample.java
@@ -0,0 +1,37 @@
+package concurrency.thread_pools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class WorkerThread implements Runnable {
+
+    private final int taskId;
+
+    public WorkerThread(int taskId) {
+        this.taskId = taskId;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " is executing task " + taskId);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println("Task " + taskId + " was interrupted");
+        }
+        System.out.println(Thread.currentThread().getName() + " has completed task " + taskId);
+    }
+}
+
+public class ThreadPoolExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 1; i <= 5; i++) {
+            WorkerThread worker = new WorkerThread(i);
+            executorService.submit(worker);
+        }
+
+        executorService.shutdown();
+    }
+}
